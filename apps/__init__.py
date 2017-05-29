839a4e3cf01a725d599de61560969ebc3136b4a8
hrkim667 <hrkim667@gmail.com>
2017-05-29 23:14:38 +0900

    Create login/logout function

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, url_for, redirect
 
 app = Flask(__name__)
+app.secret_key =b'JSz\xf8\x1cR\xdd\x04\xbf>\x15d\x02\x8c\x08\xc0*\x8f?d\x89:\x0b\x87'
+def confirmLogin():
+    try:
+        if session["user"] == "<User hrkim, hongrae>":
+            return True
+        else:
+            return False
+    except:
+        return False
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", login=confirmLogin())
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if confirmLogin() == True:
+        return redirect(url_for("index"))
+    if request.method == 'POST':
+        id = request.form["id"]
+        pw = request.form["pw"]
+        if id == "hrkim" and pw == "hongrae":
+            session["user"] = "<User {0}, {1}>".format(id, pw)
+            return redirect(url_for("index"))
+        else:
+            return render_template("login.html", login=confirmLogin(), message="Please try again")
+    return render_template("login.html", login=confirmLogin(), message="")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("index"))
